ca579fcbbecf3ffc2c867d198049d111abbac7dc
Josanghyeon <lokijoji4@gmail.com>
2022-07-18 13:59:04 +0900

    (#1) feat: webpack, babel, react, typescript setting

---
+++ config/webpack/webpack.common.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const paths = require('./paths.js');
+const getClientEnvironment = require('./env.js');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+
+const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1));
+
+module.exports = {
+  entry: paths.appIndex,
+  output: {
+    path: paths.appBuild,
+    assetModuleFilename: 'static/media/[hash][ext][query]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(svg|jpg|png)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CaseSensitivePathsPlugin(),
+    new HtmlWebpackPlugin({
+      template: paths.appHtml,
+      filename: 'index.html',
+      templateParameters: env.raw,
+    }),
+    new webpack.DefinePlugin(env.stringified),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import NOCRA from './NOCRA.png';
+
+function App() {
+  var b =  1;
+  return (
+    <div>
+      <strong>
+        This is the default template. Customize it from now on Now Mode is
+        {process.env.APP_ENV}
+      </strong>
+      <img src={NOCRA} alt="NOCRA IMG" />
+    </div>
+  );
+}
+
+export default App;
